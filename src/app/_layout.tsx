50cc20422c97b37da5f31542fb90a03ccda55d65
Caroline Vieira <carolinesanvieira@gmail.com>
2024-04-02 23:43:04 +0200

    feat: add google fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,24 @@
-import { Slot } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import "../styles/global.css";
 
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+import {
+	Roboto_400Regular,
+	Roboto_500Medium,
+	Roboto_700Bold,
+	useFonts
+} from "@expo-google-fonts/roboto";
 
 export default function Layout(){
+	const [fontLoaded] = useFonts({
+		Roboto_400Regular,
+		Roboto_500Medium,
+		Roboto_700Bold
+	})
+	if (!fontLoaded){
+		return
+	}
 	return(
 		<>
 		<StatusBar style="light"/>
