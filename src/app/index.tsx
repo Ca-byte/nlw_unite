ff5e33149055c11466877ba4d2bfa3d3046eb924
Caroline Vieira <carolinesanvieira@gmail.com>
2024-04-02 22:59:39 +0200

    configure nativewind

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default function Home(){
+	return(
+		<View style={{ backgroundColor: 'gray', flex: 1}}>
+			<Text>Hello Peach!</Text>
+		</View>
+	)
+}
