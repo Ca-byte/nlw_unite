cac7cff83d41c74a1f5c056cfb20c0871c4a064a
Caroline Vieira <carolinesanvieira@gmail.com>
2024-04-04 15:14:40 +0200

    feat: add reanimation and moti

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -2,12 +2,14 @@ import { BadgeStore } from "@/app/store/badge-store";
 import { QRCode } from "@/components/qrcode";
 import { colors } from "@/styles/colors";
 import { Feather } from "@expo/vector-icons";
+import { MotiView } from "moti";
 import {
 	Image,
 	ImageBackground,
 	Text,
 	TouchableOpacity,
-	View
+	View,
+	useWindowDimensions
 } from "react-native";
 
 type Props = {
@@ -17,8 +19,35 @@ type Props = {
 }
 
 export function Credential({onChangeAvatar, onExpandQRCode, data}: Props){
+	const { height } =useWindowDimensions()
 	return(
-		<View className="w-full self-stretch items-center">
+		<MotiView 
+			className="w-full self-stretch items-center"
+			from={{
+				opacity: 1,
+				translateY: -height,
+				rotateZ: "50deg",
+				rotateX: "30deg",
+				rotateY: "30deg"
+
+			 }}
+			animate={{
+				opacity: 1,
+				translateY: 0,
+				rotateZ: "0deg",
+				rotateX: "0deg",
+				rotateY: "0deg"
+
+			}}
+			transition={{
+				type: "spring",
+				damping: 20,
+				rotateZ: {
+					damping: 15,
+					mass: 3
+				}
+			}}
+			>
 			<Image 
 				source={require("@/assets/ticket/band.png")}
 				className="w-24 h-52 z-10"
@@ -76,7 +105,6 @@ export function Credential({onChangeAvatar, onExpandQRCode, data}: Props){
 					</TouchableOpacity>
 
 			</View>
-
-		</View>
+		</MotiView>
 	)
 }
